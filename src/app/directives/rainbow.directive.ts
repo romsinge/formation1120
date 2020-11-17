70a474604ee2783c5f85fba4954e6220b1318650
rom <romain.signes@gmail.com>
2020-11-17 10:47:25 +0100

    custom directive

--- src/app/directives/rainbow.directive.ts
+++ src/app/directives/rainbow.directive.ts
@@ -1,12 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { PoneyData } from '../models/poney-data.model';
 
 @Directive({
   selector: '[ambRainbow]'
 })
 export class RainbowDirective {
 
-  constructor() {
-    console.log('DIRECTIVE CONSTRUCTOR')
+  @Input() color: string = 'silver'
+  @Input('ambRainbow') isColored: boolean = false
+  @Input() poneyData: PoneyData
+
+  constructor(private element: ElementRef) {}
+
+  ngOnInit() {
+    if (this.isColored) {
+      this.element.nativeElement.style.backgroundColor = this.color
+    }
+  }
+
+  @HostListener('dblclick')
+  handleDblclick() {
+    this.poneyData.distance += 10
   }
 
 }
+
